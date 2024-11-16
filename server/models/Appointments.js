fb0db7facb3a8d1eeb58c67a81df62e5a2223ac7
cnacc23 <cicivgn02@gmail.com>
2024-11-16 17:10:41 -0500

    prelim setting up db

---
+++ server/models/Appointments.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const AppointmentsSchema = new mongoose.Schema({
+    hostId: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Users",
+        required: true
+    }, 
+    title: {
+        type: String, 
+        default: ''
+    }, 
+    date: {
+        type: Date, 
+        required: true
+    }, 
+    startTime: {
+        type: String, 
+        required: true
+    }, 
+    endTime: {
+        type: String, 
+        required: true
+    }, 
+    url: {
+        type: String, 
+        required: [true, 'Please add the booking url.']
+    }, 
+    maxNumParticipants: {
+        type: Number, 
+        default: 1
+    }, 
+    bookings: [
+        {
+            requesterId: {
+                type: mongoose.Schema.Types.ObjectId, 
+                ref: "Users", 
+                required: true
+            }, 
+            bookingTime:{
+                type: Date, 
+                default: Date.now
+            }
+        }
+    ], 
+    createdAt: {
+        type: Date, 
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model('Appointments', AppointmentsSchema);
---
+++ server/models/Users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose'); 
+
+const UsersSchema = new mongoose.Schema({
+    firstName: {
+        type: String, 
+        required: [true, 'Please add a first name.']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Please add a last name.']
+    }, 
+    email: {
+        type: String, 
+        required: [true, 'Please add an email.']
+    }, 
+    password: {
+        type: String, 
+        required: [true, 'Please add a password.']
+    }
+});
+
+module.exports = mongoose.model('Users', UsersSchema);
