765e82d5e6419e9fd04b44f5f5d72d20d9f32962
Ella Young <ellasyoung@gmail.com>
2024-11-29 00:37:49 -0500

    Finished confirmation popup for weekly

--- client/src/components/RecurringWeeklyModal/index.js
+++ client/src/components/RecurringWeeklyModal/index.js
@@ -14,13 +14,29 @@ import {
     TitleInput,
     FormContainer,
     TitleContainer,
+    Dim,
+    ConfirmationModal,
+    CloseButton,
+    ModalTitle,
+    ModalText,
+    ModalLink,
+    Line,
+    SendIcon,
 } from './RecurringWeeklyModalElements';
 import axios from 'axios';
 import { AuthContext } from '../../auth/AuthProvider'; 
+import { FaAngleRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const RecurringWeeklyModal = () => {
 
     const { email } = useContext(AuthContext); 
+    const [isConfirmed, setIsConfirmed] = useState(false);
+    const [confirmationDetails, setConfirmationDetails] = useState(null);
+
+    const toggleConfirmation = () => {
+        setIsConfirmed(!isConfirmed);
+      };
 
     const [formData, setFormData] = useState({
         startDate: '',
@@ -73,9 +89,9 @@ const RecurringWeeklyModal = () => {
             };
 
             const response = await axios.post('http://localhost:5001/api/new-weekly-meeting', requestData);
-
+            setConfirmationDetails(response.data); 
             console.log('Meeting created successfully:', response.data);
-            alert('Recurring weekly meeting created successfully!');
+            //alert('Recurring weekly meeting created successfully!');
 
             setFormData({
                 hostEmail: '',
@@ -87,6 +103,7 @@ const RecurringWeeklyModal = () => {
                 endTime: '',
                 title: '',
             });
+            toggleConfirmation();
         } catch (error) {
             console.error('Error creating weekly meeting:', error.response?.data || error.message);
             alert('There was an error creating the meeting. Please try again.');
@@ -206,6 +223,27 @@ const RecurringWeeklyModal = () => {
                     </Form>
                 </InnerModal>
             </ModalContainer>
+            {isConfirmed && (
+            <Dim>
+                <ConfirmationModal>
+                <CloseButton onClick={toggleConfirmation} />
+                <ModalTitle>Meeting Created!</ModalTitle>
+                <ModalText>                
+                    {`This meeting will occur on the first ${confirmationDetails.schedule.dayOfWeek} of each month at ${new Date(`1970-01-01T${confirmationDetails.schedule.startTime}`).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}-${new Date(`1970-01-01T${confirmationDetails.schedule.endTime}`).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })} from ${new Date(confirmationDetails.schedule.startDate).toLocaleDateString()} until ${new Date(confirmationDetails.schedule.endDate).toLocaleDateString()}.`}
+                </ModalText>
+                <Line>
+                    <FormGroup className='conf'>
+                        <Label className='label'>Booking Link: </Label>
+                        <ModalLink href='https://www.google.com' target='_blank'>                
+                            {`https://www.socsconnect.com/wsetdrytfuygiu23456789`}
+                        </ModalLink>
+                    </FormGroup>
+                    <SendIcon link="https://www.socsconnect.com/wsetdrytfuygiu23456789"/>
+                </Line>
+                <Button className='seeApts' as={Link} to="/my-appointments">See Your Appointments <FaAngleRight size="1em" style={{ marginLeft: "8px" }}/></Button>
+                </ConfirmationModal>
+            </Dim>
+            )}
         </Bckgrnd>
     );
 };
