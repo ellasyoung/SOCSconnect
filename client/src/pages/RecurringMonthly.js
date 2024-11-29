a200b40cc02249c41f6a84738650180a1d9c1d4f
ndoehla <119361524+ndoehla@users.noreply.github.com>
2024-11-29 17:11:45 -0500

    recurring monthly page

---
+++ client/src/pages/RecurringMonthly.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PrivNavbar from "../components/PrivNavbar";
+import RecurringWeeklyModal from '../components/RecurringMonthlyModal';
+import Footer from "../components/Footer";
+
+const RecurringMonthly = () => {
+  return(
+    <>
+      <PrivNavbar/>
+      <RecurringWeeklyModal/>
+      <Footer/>
+    </>
+  );
+};
+
+export default RecurringMonthly;
