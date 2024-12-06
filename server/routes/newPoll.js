35d41023c379349b6b543761fa4d6d65ecf1bda9
Ella Young <ellasyoung@gmail.com>
2024-12-05 21:59:12 -0500

    Getting votes

--- server/routes/newPoll.js
+++ server/routes/newPoll.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 const Users = require('../models/Users');
 const Polls = require('../models/Polls');
+const mongoose = require('mongoose');
 
 router.post('/polls/:pollId/vote', async (req, res) => {
     const { pollId } = req.params;
@@ -40,6 +41,59 @@ router.post('/polls/:pollId/vote', async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'An error occurred while saving votes' });
+    }
+});
+
+router.get('/polls/:pollId/vote-count', async (req, res) => {
+    const { pollId } = req.params;
+    const { date, startTime, endTime } = req.query;
+
+    if (!pollId || !date || !startTime || !endTime) {
+        return res.status(400).json({ message: 'Please provide pollId, date, startTime, and endTime' });
+    }
+
+    try {
+        // Normalize the query date to midnight UTC
+        const queryDate = new Date(date);
+        const startOfDay = new Date(Date.UTC(queryDate.getUTCFullYear(), queryDate.getUTCMonth(), queryDate.getUTCDate()));
+        const endOfDay = new Date(startOfDay);
+        endOfDay.setUTCDate(startOfDay.getUTCDate() + 1);
+
+        console.log('Start of Day:', startOfDay);
+        console.log('End of Day:', endOfDay);
+
+        const poll = await Polls.aggregate([
+            {
+                $match: {
+                    _id: new mongoose.Types.ObjectId(pollId),
+                    'pollOption.date': { $gte: startOfDay, $lt: endOfDay }
+                }
+            },
+            { $unwind: '$pollOption' },
+            { $unwind: '$pollOption.timeOptions' },
+            {
+                $match: {
+                    'pollOption.timeOptions.startTime': startTime,
+                    'pollOption.timeOptions.endTime': endTime
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    voteCount: { $size: '$pollOption.timeOptions.votes' }
+                }
+            }
+        ]);
+
+        if (!poll.length) {
+            console.error('No matching poll or time slot found for:', { date, startTime, endTime });
+            return res.status(404).json({ message: 'No poll or time slot found' });
+        }
+
+        res.json({ voteCount: poll[0].voteCount });
+    } catch (error) {
+        console.error('Error fetching vote count:', error);
+        res.status(500).json({ message: 'An error occurred', error });
     }
 });
 
